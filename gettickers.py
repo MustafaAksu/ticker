973f68d0ac988d16971a10743bea71af8df23031
MustafaAksu <mustafa_aksu@yahoo.com>
2025-11-07 15:38:35 +0300

    longer period

--- gettickers.py
+++ gettickers.py
@@ -3,14 +3,17 @@ import yfinance as yf
 import pandas as pd
 
 tickers = [
-    'THYAO.IS','GARAN.IS','AKBNK.IS','ISCTR.IS','YKBNK.IS',
-    'ASELS.IS','EREGL.IS','SISE.IS','PETKM.IS','TUPRS.IS',
-    'SAHOL.IS','KCHOL.IS','BIMAS.IS','TTKOM.IS','ARCLK.IS',
-    'FROTO.IS','VESTL.IS','TCELL.IS','HALKB.IS','ENKAI.IS'
+    'ASELS.IS','GARAN.IS','ENKAI.IS','KCHOL.IS','THYAO.IS','TUPRS.IS','ISCTR.IS','FROTO.IS',
+    'BIMAS.IS','AKBNK.IS','YKBNK.IS','DSTKF.IS','TCELL.IS','TTKOM.IS','EREGL.IS','SAHOL.IS',
+    'SASA.IS','TOASO.IS','SISE.IS','PGSUS.IS','GUBRF.IS','ASTOR.IS','TAVHL.IS','AEFES.IS',
+    'KOZAL.IS','EKGYO.IS','PETKM.IS','ULKER.IS','KRDMD.IS','MGROS.IS',
+    'AEFES.IS','ALARK.IS','ASTOR.IS','DOHOL.IS','DOAS.IS','DSTKF.IS',
+    'EKGYO.IS','GUBRF.IS','KRDMD.IS','KOZAL.IS','MGROS.IS','PGSUS.IS',
+    'SASA.IS','TAVHL.IS','TOASO.IS','ULKER.IS'
 ]
 
 print("BIST100 verisi indiriliyor...")
-data = yf.download(tickers, period='2y', interval='1d', auto_adjust=False)
+data = yf.download(tickers, period='5y', interval='1d', auto_adjust=False)
 
 # Close ve Volume'u düzgün al
 close = data['Close']      # (tarih, ticker)
